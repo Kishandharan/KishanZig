dc0c1a2d22acd7bc32dc190a3026cc3b170e3f55
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-02-09 14:52:55 +0530

    Fixed errors, but program still not complete

--- 4.zig
+++ 4.zig
@@ -1,14 +1,14 @@
-var std = @import("std");
+const std = @import("std");
 
 pub fn main() !void{
     for(0..100) |i|{
-        if(i%3 == true){
+        if(i%3 == 0){
             std.debug.print("fizz", .{});
         }
-        if((i%3 == true) and (i%5 == true){
+        if((i%3 == 0) and (i%5 == 0)){
             std.debug.print("fizzbuzz", .{});
         }
-        if(i%5 == true){
+        if(i%5 == 0){
             std.debug.print("buzz", .{});
         }
     }
